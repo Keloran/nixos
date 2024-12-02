b066f8451592082278ccedad32ad4bc95ca59237
keloran <keloran@chewedfeed.com>
2024-12-02 14:23:10 +0000

    updated git config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -36,6 +36,22 @@
     };
     git = {
       enable = true;
+      userName = "keloran";
+      userEmail = "keloran@chewedfeed.com";
+      extraConfig = {
+        gpg = {
+	  format = "ssh";
+	};
+	"gpg \"ssh\"" = {
+	  program = "${lib getExe' pkgs._1password-gui "op-ssh-sign"}";
+	};
+	commit = {
+	  gpgsign = true;
+	};
+	user = {
+	  signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxKs7zVMUjUHGzMLNAQA3GTRzQIucJZkrvhFsaRvdAw";
+	};
+      };
     };
     home-manager = {
       enable = true;
